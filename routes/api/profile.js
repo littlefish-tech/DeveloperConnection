9fec23c9aec52cc33c47499b85ef29ef85080202
Yimeng Yu <yimeng.yu@gmail.com>
2020-05-11 12:20:41 -0700

    get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,11 +4,26 @@
 const express = require("express");
 // to use express router
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // create route, (req, res) => res.send("User route") is the call back function
-//@route   GET api/profile
-// @ does  test route
-// @access Public, doesn't need a token
-router.get("/", (req, res) => res.send("Profile route"));
+//@route   GET api/profile/me
+// @ does  get current users profile
+// @access  private
+router.get("/me", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate("user", ["name", "avatar"]);
+
+        if (!profile) {
+            return res.status(400).json({ msg: "There is no profile for this user" });
+        }
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 module.exports = router;

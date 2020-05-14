ac0deda1eac5c65b6bc1d5116cad994ede0e194a
Yimeng Yu <yimeng.yu@gmail.com>
2020-05-13 19:06:18 -0700

    unlike comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -107,6 +107,49 @@ router.delete("/:id", auth, async (req, res) => {
         console.error(err.message);
         res.status(404).json({ msg: "post not found" })
     }
-})
+});
+
+// @route put api/posts/like/:id;
+// @desc like post
+// @access private
+
+router.put("/like/:id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: "Post already liked" })
+        }
+        post.likes.unshift({ user: req.user.id })
+        await post.save();
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error")
+    }
+});
+
+// @route  api/post/unlike/:id;
+// @desc unlike a post
+// @access private
+
+router.put("/unlike/:id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: "Post has not been unliked " });
+        }
+        // get remove index
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+        post.likes.splice(removeIndex, 1)
+        await post.save();
+
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status.send("Server Error")
+    }
+});
+
+
 
 module.exports = router;

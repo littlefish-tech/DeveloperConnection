35b4f4b6f4437c28140782cfb928eac81a265266
Yimeng Yu <yimeng.yu@gmail.com>
2020-05-15 02:47:48 -0700

    set up redux store

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,26 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import store from "./store"
 
 const App = () => {
   return (
-    <Router>
-      {/* fragment will not be showing up on the DOM */}
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-      </Fragment >
-    </Router >
+    <Provider store={store}>
+      <Router>
+        {/* fragment will not be showing up on the DOM */}
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment >
+      </Router >
+    </Provider>
   );
 }
 
